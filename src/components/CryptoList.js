4fd4deeb90e6dbbedd3a2047e4db71fdfb0871e2
Janko <jasofalcon@gmail.com>
2018-02-12 23:18:47 +0100

    add first graph

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchCrypto } from '../actions/index';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class CryptoList extends Component {
 
@@ -10,16 +11,23 @@ class CryptoList extends Component {
     }
 
     render() {
+        if (!this.props.cryptos) return <div></div>;
+
+        console.log();
         return (
-            <div>
-                Hi from components
+            <div style={{width:'300px', height :'300px'}}>
+                <Sparklines data={this.props.cryptos.BTC} limit={12} width={100} height={20} margin={5}>
+                    <SparklinesLine color="blue" />
+                </Sparklines>
             </div>
         )
     }
 }
 
+const mapStateToProps = ({ cryptos }) => ({ cryptos })
+
 function mapDispatchToProps(dispatch) {
     return bindActionCreators({ fetchCrypto }, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(CryptoList);
+export default connect(mapStateToProps, mapDispatchToProps)(CryptoList);
