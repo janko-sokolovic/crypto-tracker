8b404cb38f56900fd1278b79914d1c494676cae5
Janko <jasofalcon@gmail.com>
2018-02-12 16:35:41 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from 'antd/lib/button';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+        <Button type="primary">Button</Button>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
