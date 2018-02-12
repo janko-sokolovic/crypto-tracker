fe6711dbf3f086ae1523403ccbd931189b361bf4
Janko <jasofalcon@gmail.com>
2018-02-12 22:34:46 +0100

    refactor url creation

---
+++ src/actions/CoinApiUrlFactory.js
@@ -0,0 +1,23 @@
+
+const BASE_URL = 'https://rest.coinapi.io/v1/ohlcv';
+const API_KEY = 'apiKey=F8F4A75C-6E67-4287-9C6E-040282572AFC';
+const PERIOD = '2HRS';
+
+export default class CoinApiUrlFactory {
+
+    /**
+     * @param {string} currencyId | simple id for currency, like ETH, BTC,..
+     */
+    static create(currencyId) {
+        let yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+
+        // this is starting period when we are fetching data
+        // set to 1 day before => 12 items
+        const startTime = `time_start=${yesterday.toISOString()}`;
+
+        const period = `period_id=${PERIOD}`;
+
+        return `${BASE_URL}/COINBASE_SPOT_${currencyId}_USD/history?${period}&${startTime}&${API_KEY}`
+    }
+}
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,31 +1,18 @@
 import axios from 'axios';
 
-const API_KEY = 'apiKey=F8F4A75C-6E67-4287-9C6E-040282572AFC';
-
-const BASE_URL = 'https://rest.coinapi.io/v1/ohlcv';
-
-const PERIOD = '2HRS';
+import CoinApiUrlFactory from './CoinApiUrlFactory';
 
 export const ACTIONS = {
     FETCH_CRYPTO: 'FETCH_CRYPTO'
 }
 
+//Action-creators 
 export function fetchCrypto(name) {
-    let yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
 
-    // this is starting period when we are fetching data
-    // set to 1 day before => 12 items
-    const startTime = `time_start=${yesterday.toISOString()}`;
-
-    const period = `period_id=${PERIOD}`
-
-    const url = `${BASE_URL}/COINBASE_SPOT_${name}_USD/history?${period}&${startTime}&${API_KEY}`
+    const url = CoinApiUrlFactory.create(name);
 
     const request = axios.get(url, {
-        headers: {
-            'Accept': 'application/json'
-        }
+        headers: { 'Accept': 'application/json' }
     });
 
     return {
