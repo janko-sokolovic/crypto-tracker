a0861478a28ce48d38460b8b659b2e896f04d2e6
Janko <jasofalcon@gmail.com>
2018-02-12 23:56:03 +0100

    add limits - hardcoded for now

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -14,8 +14,15 @@ class CryptoList extends Component {
         if (!this.props.cryptos) return <div></div>;
 
         return (
-            <div style={{ width: '300px', height: '300px' }}>
-                <Sparklines data={this.props.cryptos.BTC} limit={12} width={100} height={20} margin={5}>
+            <div style={{ width: '300px' }}>
+                <Sparklines data={this.props.cryptos.BTC}
+                    limit={12}
+                    width={150}
+                    height={50}
+                    margin={5}
+                    max={9000}
+                    min={8000}
+                >
                     <SparklinesLine color="#aa55aa" />
                 </Sparklines>
             </div>
