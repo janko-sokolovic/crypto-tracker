f01fd5387cb5a9a0a9d607ce4fd67118860a9cd6
Janko <jasofalcon@gmail.com>
2018-02-13 16:17:48 +0100

    add switch

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import CryptoList from './components/CryptoList';
 import AddCrypto from './components/AddCrypto';
 
-import Route from 'react-router-dom/Route';
-import {Link} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const { Header, Content } = Layout;
 class App extends Component {
@@ -23,11 +23,12 @@ class App extends Component {
               <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
               <Menu.Item key="2"><Link to="/add-crypto">Add crypto</Link></Menu.Item>
             </Menu>
-
           </Header>
           <Content style={{ padding: '0 50px', margin: '20px 0' }}>
-            <Route path="/" component={CryptoList}></Route>
-            <Route path="/add-crypto" component={AddCrypto}></Route>
+            <Switch>
+              <Route path="/add-crypto" component={AddCrypto}></Route>
+              <Route path="/" component={CryptoList}></Route>
+            </Switch>
           </Content>
         </Layout>
       </div>
